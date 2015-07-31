a2d84394c76067e4d440f7a57ad1b51617568d41
Mario Finelli <mario@finel.li>
2015-07-30 20:36:10 -0400

    Create defined type to write out known hosts

---
+++ manifests/known_host.pp
@@ -0,0 +1,30 @@
+define known_hosts::known_host (
+  $ensure  = 'present',
+  $user    = undef,
+  $revoked = false,
+  $host_aliases = [],
+  $key = undef,
+  $type = undef
+) {
+
+  if $user == undef {
+    $target = '/etc/ssh/ssh_known_hosts'
+  } else {
+    $target = "/home/${user}/.ssh/known_hosts"
+  }
+
+  if $revoked {
+    $revoked_str = '@revoked '
+  } else {
+    $revoked_str = ''
+  }
+
+  $aliases = join(concat(any2array($name), $host_aliases), ',')
+
+  concat::fragment { "${name} known host for ${user}":
+    order => '50',
+    target => $target,
+    content => "${revoked_str}${aliases} ${type} ${key}\n"
+  }
+
+}
