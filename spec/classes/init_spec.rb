d65258bbb441882ee003b50a68e849873acc0cda
Mario Finelli <mario@finel.li>
2015-07-31 07:52:01 -0400

    Add concat basedir

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 describe 'known_hosts' do
 
   context 'with defaults for all parameters' do
+    let(:facts) { {:concat_basedir => '/tmp'} }
     it { should contain_class('known_hosts') }
   end
 end
